b239d06a659e3c7534c76847da98bf258be373b6
Jason Kwon <jasonproject628@gmail.com>
2022-10-08 10:19:31 +0900

    Anonymous Class

---
+++ JAVA/Anonymous_Class.java
@@ -0,0 +1,27 @@
+class Machine {
+    public void start() {
+        System.out.println("Starting...");
+    }
+    public void Winging() {
+        System.out.println("Wing-Wing-Wing...");
+    }
+}
+
+class Program {
+    public static void main(String[ ] args) {
+        Machine m1 = new Machine() {
+            @Override public void start() {
+                System.out.println("Wooooo");
+            }
+        };
+        Machine m2 = new Machine() {
+            @Override public void Winging() {
+                System.out.println("Wang-Wang-Wang");
+            }
+        };
+        m1.start();
+        m2.start();
+        m1.Winging();
+        m2.Winging();
+    }
+}
