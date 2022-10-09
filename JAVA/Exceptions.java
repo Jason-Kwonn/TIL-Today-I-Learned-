0efc0519aee91762c1b4789ae88d74af5d5754e5
Jason Kwon <jasonproject628@gmail.com>
2022-10-09 15:49:19 +0900

    Try_Catch(Exception)

---
+++ JAVA/Exceptions.java
@@ -0,0 +1,28 @@
+/* 
+0. Import a new Scanner
+1. Create Exception class
+2. Create a new Scanner in the main method()
+3. Crt some arr
+4. try, catch(Exception )
+*/
+
+import java.util.Scanner;
+
+public class Exceptions {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        
+        int[] categories = {11, 22, 33, 44, 55, 66};
+
+        try {
+            System.out.println(categories[choice]);
+        }
+        catch(Exception integer) {
+            System.out.println("Caterogy Exception Occured.");
+        }
+        
+        scanner.close();
+    }
+}
