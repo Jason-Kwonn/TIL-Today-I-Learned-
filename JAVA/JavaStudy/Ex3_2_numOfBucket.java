99ca2eaf910f7bdcf81081ffdccf02d4d4143d5e
Jason Kwon <jasonproject628@gmail.com>
2022-12-19 05:53:29 +0900

    연산 예제

---
+++ JAVA/JavaStudy/Ex3_2_numOfBucket.java
@@ -0,0 +1,15 @@
+package JavaStudy;
+
+public class Ex3_2_numOfBucket {
+
+    public static void main(String[] args) {
+        int numOfApples = 123;
+        int sizeOfBucket = 10;
+        int numOfBucket = (numOfApples/sizeOfBucket);
+        if (numOfApples % sizeOfBucket > 0){
+            numOfBucket++;
+        }
+        System.out.println("필요한 바구니의 수 = " + numOfBucket);
+
+    }
+}
