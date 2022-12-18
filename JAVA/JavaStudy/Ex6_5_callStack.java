e81fa0c9f18f9614d4bf025b8bdd2e5b6aba00bc
Jason Kwon <jasonproject628@gmail.com>
2022-12-19 05:53:57 +0900

    call stack

---
+++ JAVA/JavaStudy/Ex6_5_callStack.java
@@ -0,0 +1,12 @@
+package JavaStudy;
+
+public class Ex6_5_callStack {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        String call = "Call";
+        String stack = "Stack";
+
+        System.out.println("호출 스택 = " + call + " " + stack);
+    }
+}
