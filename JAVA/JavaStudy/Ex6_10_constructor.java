f705f69f70acf2bf75fcc8703a2ef507699555a0
Jason Kwon <jasonproject628@gmail.com>
2022-12-21 07:16:18 +0900

    Constructors

---
+++ JAVA/JavaStudy/Ex6_10_constructor.java
@@ -0,0 +1,27 @@
+package JavaStudy;
+
+public class Ex6_10_constructor {
+
+}
+
+class Car {
+    String color;    // 색상
+    String gearType; // 변속기 종류 - auto(자동), manual(수동)
+    int door;        // 문의 개수
+
+    Car(){} //기본 생성자
+
+    Car(String c, String g, int d) { // 매개변수가 있는 생성자
+        color = c;
+        gearType = g;
+        door = d;
+
+        Car car = new Car("white", "auto", 4);
+    }
+/*
+1. 참조변수 car 생성 : Car c
+2. new 연산자로 객체 생성 : new
+3. 생성자 호출 Car(매개변수, 매개변수, Parameter)
+4. 대입 : Car c = new Car(...);
+ */
+}
---
+++ JAVA/JavaStudy/Ex6_11_constructorThis.java
@@ -0,0 +1,30 @@
+package JavaStudy;
+
+public class Ex6_11_constructorThis {
+
+    public static void main(String[] args) {
+        CarSpec carSpec1 = new CarSpec();
+        CarSpec carSpec2 = new CarSpec("black", "manual", 2);
+        System.out.println(carSpec1.color);
+        System.out.println(carSpec2.gearType);
+    }
+}
+
+class CarSpec {
+    String color;
+    String gearType;
+    int door;
+
+    CarSpec(String c, String g, int d) {
+        color = c;
+        gearType = g;
+        door = d;
+    }
+
+    CarSpec() {
+        this("white", "auto", 4); // 코드 중복 없애기
+        color = "white";
+        gearType = "auto";
+        door = 4;
+    }
+}
