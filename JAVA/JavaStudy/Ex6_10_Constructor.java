d79a596e28ba6f4d5da1111105b89b2e096f30bd
Jason Kwon <jasonproject628@gmail.com>
2022-12-29 06:34:17 +0900

    .

--- JAVA/JavaStudy/Ex6_10_Constructor.java
+++ JAVA/JavaStudy/Ex6_10_Constructor.java
@@ -4,24 +4,24 @@ public class Ex6_10_Constructor {
 
 }
 
-class Car {
+class Car1 {
     String color;    // 색상
     String gearType; // 변속기 종류 - auto(자동), manual(수동)
     int door;        // 문의 개수
 
-    Car(){} //기본 생성자
+    Car1(){} //기본 생성자
 
-    Car(String c, String g, int d) { // 매개변수가 있는 생성자
+    Car1(String c, String g, int d) { // 매개변수가 있는 생성자
         color = c;
         gearType = g;
         door = d;
 
-        Car car = new Car("white", "auto", 4);
+        Car1 car1 = new Car1("white", "auto", 4);
     }
 /*
-1. 참조변수 car 생성 : Car c
+1. 참조변수 car1 생성 : Car1 c
 2. new 연산자로 객체 생성 : new
-3. 생성자 호출 Car(매개변수, 매개변수, Parameter)
-4. 대입 : Car c = new Car(...);
+3. 생성자 호출 Car1 (매개변수, 매개변수, Parameter)
+4. 대입 : Car1 c = new Car(...);
  */
 }
