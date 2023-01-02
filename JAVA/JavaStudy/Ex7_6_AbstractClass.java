003f0b2b92cc3392a0dba2a240f86ec7eec9f596
Jason Kwon <jasonproject628@gmail.com>
2023-01-03 04:54:41 +0900

    Abstract class, method (ex. Starcraft)

--- JAVA/JavaStudy/Ex7_6_AbstractClass.java
+++ JAVA/JavaStudy/Ex7_6_AbstractClass.java
@@ -1,6 +1,22 @@
 package JavaStudy;
 
 public class Ex7_6_AbstractClass {
+    public static void main(String[] args) {
+        Unit[] group = {new Marine(), new Tank(), new Dropship()};
+//        Unit[] group = new Unit[3]; // 위 1줄과 같은 코드 (객체 = 참조변수의 배열)
+//        Object 에는 move 가 없어서 Object 로는 생성 불가
+//        group[0] = new Marine();
+//        group[1] = new Tank();
+//        group[2] = new Dropship();
+
+
+        for (int i = 0; i < group.length; i++)
+            group[i].move(100, 200);
+        // group 의 타입은 Unit[], group[0], group[1], group[2] 의 타입은 Unit
+//        group[0].move(100, 200); // Marine 객체의 move(100, 200) 을 호출
+//        group[1].move(100, 200); // Tank 객체의 move(100, 200) 을 호출
+//        group[2].move(100, 200); // Dropship 객체의 move(100, 200) 을 호출
+    }
 }
 
 abstract class Unit {
@@ -15,6 +31,7 @@ abstract class Unit {
 class Marine extends Unit { //보병
     int x, y; // 현재 위치
     void move(int x, int y) {
+        System.out.println("Marine[x = " + x + ",y = " + y + "]");
         /* 지정된 위치로 이동 */
     }
 
@@ -26,6 +43,7 @@ class Marine extends Unit { //보병
 class Tank extends Unit { // 탱크
     int x, y; // 현재 위치
     void move(int x, int y) {
+        System.out.println("Tank[x = " + x + ",y = " + y + "]");
         /* 지정된 위치로 이동 */
     }
 
@@ -37,6 +55,7 @@ class Tank extends Unit { // 탱크
 class Dropship extends Unit { //수송선
     int x, y; // 현재 위치
     void move(int x, int y) {
+        System.out.println("Dropship[x = " + x + ",y = " + y + "]");
         /* 지정된 위치로 이동 */
     }
 
