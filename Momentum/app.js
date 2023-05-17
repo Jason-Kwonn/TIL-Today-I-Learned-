9cc4e074b62f4487b477c31d16bced568491cdac
Jason Kwon <jasonproject628@gmail.com>
2023-05-17 14:40:37 +0900

    const and let

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,9 +1,17 @@
+/* 
+const can't be changed
+let can be changed
+*/
 const a = 10;
 const b = 2;
 
-const myName = "jason";
+let myName = "jason";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
+console.log("Hello " + myName);
+
+myName = "JASON";
+
 console.log("Hello " + myName);
