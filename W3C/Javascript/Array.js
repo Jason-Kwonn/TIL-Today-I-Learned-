df40aa14389e67bdf4bc03c4d2945707f37c6ab3
Jason Kwon <jasonproject628@gmail.com>
2023-05-17 15:11:26 +0900

    Arrays

---
+++ W3C/Javascript/Array.js
@@ -0,0 +1,9 @@
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fir", "sat", "sun"];
+
+// Get item from array
+console.log(daysOfWeek[0]);
+
+// Add one more day to the array
+daysOfWeek.push("anotherday");
+
+console.log(daysOfWeek);
