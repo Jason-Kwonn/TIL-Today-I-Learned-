bc7805d2517fb0ae3374e09f64aceee0affad9b1
Jason Kwon <jasonproject628@gmail.com>
2023-05-21 19:55:28 +0900

    Function is awsome

---
+++ W3C/Javascript/Function.js
@@ -0,0 +1,10 @@
+const player = {
+  name: "jason",
+  sayHello: function (name) {
+    console.log("hello! my name is " + name);
+  },
+};
+
+console.log(player.name);
+player.sayHello(player.name);
+// Bring the player's name jason
